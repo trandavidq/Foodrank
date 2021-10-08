6a1f6a1a4ac53362301e0d75fd18999ca657a004
Henry Todd <htodd2001@gmail.com>
2021-10-08 15:11:59 -0400

    updated 'Holder' screen for better desc.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import Holder from "./Screens/Holder"
+import StackNavHolder from "./Screens/StackNavHolder"
 import Home from "./Screens/Home"
 import List from "./Screens/List"
 import CreatePost from "./Screens/CreatePost"
@@ -15,7 +15,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Holder" component={Holder} />
+        <Tab.Screen name="Food Categories" component={StackNavHolder} />
         <Tab.Screen name="CreatePost" component={CreatePost} />
       </Tab.Navigator>
     </NavigationContainer>
