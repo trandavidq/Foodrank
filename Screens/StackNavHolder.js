df1fba34ff76a9f53a8fa464006d6e75616944c4
silkykay <mlt6kps@virginia.edu>
2021-11-26 08:38:33 -0500

    added firebase methods, signin,signup,stacks

--- Screens/StackNavHolder.js
+++ Screens/StackNavHolder.js
@@ -9,14 +9,22 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import * as Font from 'expo-font';
 import ListScreen from './List';
-import Home from "./Home"
+import Home from "./Home";
+import CreatePost from "./CreatePost";
 import ViewPostScreen from "./ViewPost";
+import LoadingScreen from './LoadingScreen';
+import Welcome from './Welcome';
+import SignUp from './SignUp';
+import SignIn from './SignIn';
+import Dashboard from './ProfileDashboard';
 
 const Stack = createStackNavigator();
-
 export default function StackNavHolder(){
+  const [headerFont, setHeaderFont] = React.useState('Times New Roman');
     return(
       <Stack.Navigator>
         <Stack.Group screenOptions = {{headerStyle: {backgroundColor: '#F5FFFA'}}}>
